02f3d22bf03e6e0be92ebbd8f7428789c566ce10
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-01-27 17:51:03 -0500

    Add files via upload

---
+++ famous_quote.py
@@ -0,0 +1,1 @@
+print('Anthony Bourdain once said, “Maybe that’s enlightenment enough: to know that there is no final resting place of the mind; no moment of smug clarity. Perhaps wisdom…is realizing how small I am, and unwise, and how far I have yet to go”')
