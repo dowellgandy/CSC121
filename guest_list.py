c7f117b909760dc662266071c9958962ea315309
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-02-04 14:48:45 -0500

    Add files via upload

---
+++ guest_list.py
@@ -0,0 +1,6 @@
+guests = ['Carl Sagan', 'Glynn Johns', 'Steve Jobs', 'Philip Glass']
+
+print(f"Dear {guests[0]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[1]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[2]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[3]}, you are formally invited to dinner on Friday February 9 at my house")
