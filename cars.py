e2added14dd1c34c70ec63a9d7ff1cf7b8d7819d
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-02-04 13:59:12 -0500

    Add files via upload

---
+++ cars.py
@@ -0,0 +1,14 @@
+cars = ['subaru', 'jeep', 'bmw', 'ford']
+
+message1 = f"I currently drive a {cars[0]}"
+
+message2 = f"I used to drive a {cars[1]}"
+
+message3 = f"And then I drove a {cars[2]}"
+
+message4 = f"But I've never driven a {cars[3]}"
+
+print(message1)
+print(message2)
+print(message3)
+print(message4)
