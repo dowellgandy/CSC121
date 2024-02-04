1a31431416e56ba3f5b54b60781f3fb733915a81
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-02-04 15:18:46 -0500

    Add files via upload

---
+++ more_guests_sorted.py
@@ -0,0 +1,19 @@
+guests = ['Carl Sagan', 'Glynn Johns', 'Steve Jobs', 'Philip Glass']
+
+print(f"Dear {guests[0]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[1]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[2]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[3]}, you are formally invited to dinner on Friday February 9 at my house")
+print("Good news everyone! I've found a bigger table and decided to invite some more guests")
+guests.insert(0, 'David Bowie')
+guests.insert(2, 'Barack Obama')
+guests.append('Bernie Sanders')
+guests.sort()
+print(f"Dear {guests[0]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[1]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[2]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[3]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[4]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[5]}, you are formally invited to dinner on Friday February 9 at my house")
+print(f"Dear {guests[6]}, you are formally invited to dinner on Friday February 9 at my house")
+
