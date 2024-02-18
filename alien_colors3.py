cdcc4d93684600c58847fd8aad4b5436efbd249b
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-02-18 18:33:02 -0500

    Add files via upload

---
+++ alien_colors3.py
@@ -0,0 +1,8 @@
+alien_color = 'green'
+if alien_color == 'green':
+    score = 5
+elif alien_color == 'yellow':
+    score = 10
+elif alien_color == 'red':
+    score = 15
+print(f"You just earned {score} points!")    
