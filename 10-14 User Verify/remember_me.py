6ea054b6503d11671450a2e1ab22528d0f5c9f8b
Dowell Gandy <144849358+dowellgandy@users.noreply.github.com>
2024-04-01 00:11:57 -0400

    Add files via upload

---
+++ 10-14 User Verify/remember_me.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+import json
+
+def get_stored_username(path):
+    """Get stored username if available"""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+
+def get_new_username(path):
+    """Prompt for new username"""
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
+
+def greet_user():
+    """Greet the user by name"""
+    path = Path('username.json')
+    username = get_stored_username(path)
+    # username = None
+    if username:
+        userVerify = input(f"Are you {username}? Type yes if so: ")
+        if userVerify.lower() == 'yes':
+            print(f"Welcome back, {username}")
+        else:
+            username = get_new_username(path)
+    else:
+        username = get_new_username(path)
+        print(f"We'll remember you when you come back, {username}")
+
+greet_user()
+
